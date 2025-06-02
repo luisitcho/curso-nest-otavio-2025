62d05ef169be74fecd2101dfa54e96f9284347a4
luisitcho <luishenrisc1@gmail.com>
2025-06-02 08:46:24 -0300

    curso nest - Rota para atualizar um recado (update) com métodos PATCH ou PUT e decoradores

--- 1_conceitos_nest/src/recados/recados.controller.ts
+++ 1_conceitos_nest/src/recados/recados.controller.ts
@@ -5,8 +5,19 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
+
+// CRUD
+// Create -> POST -> Criar um recado
+// Read -> GET -> Ler todos os recados
+// Read -> GET -> Ler apenas um recado
+// Update -> PATCH / PUT -> Atualizar um recado
+// Delete -> DELETE -> Apagar um recado
+
+// PATCH é utilizado para atualizar dados de um recurso
+// PUT é utilizado para atualizar um recurso inteiro
 
 // Definição dos campos esperados no corpo da requisição
 interface CreateRecadoDto {
@@ -33,5 +44,13 @@ export class RecadosController {
   @Post() // /recados
   create(@Body() body: CreateRecadoDto) {
     return body;
+  }
+
+  @Patch(':id') // /recados/:id
+  update(@Param('id') id: string, @Body() body: CreateRecadoDto) {
+    return {
+      id,
+      ...body,
+    };
   }
 }
