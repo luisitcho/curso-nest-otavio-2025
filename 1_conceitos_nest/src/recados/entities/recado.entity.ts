1b3928006b7d0113ea429e21b1c9329229e25a71
luisitcho <luishenrisc1@gmail.com>
2025-06-08 05:18:43 -0300

    curso nest - Primeira Entity do TypeOrm no NestJS (primeira tabela na base de dados PSQL)

--- 1_conceitos_nest/src/recados/entities/recado.entity.ts
+++ 1_conceitos_nest/src/recados/entities/recado.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('Recado')
 export class RecadoEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   texto: string;
+
+  @Column({ type: 'varchar', length: 100 })
   de: string;
+
+  @Column({ type: 'varchar', length: 100 })
   para: string;
+
+  @Column({ type: 'boolean', default: false })
   lido: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   data: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
