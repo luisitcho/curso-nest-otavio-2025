1526c602ebe1adf9498f336329bf7c0f39dcb007
luisitcho <luishenrisc1@gmail.com>
2025-06-28 03:51:39 -0300

    curso nest - Interceptors | observando dados antes e depois da execução do método com tap

--- 1_conceitos_nest/src/common/interceptors/timing-connection.interceptor.ts
+++ 1_conceitos_nest/src/common/interceptors/timing-connection.interceptor.ts
@@ -1,14 +1,27 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class TimingConnectionInterceptor implements NestInterceptor {
   async intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Promise<Observable<any>> {
+    const startTime = Date.now();
     console.log('TimingConnectionInterceptor: Request started');
 
     await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulating a delay
-    return next.handle();
+    return next.handle().pipe(
+      tap((data) => {
+        const finalTime = Date.now();
+        const elapsed = finalTime - startTime;
+
+        console.log(
+          `TimingConnectionInterceptor: Request completed in ${elapsed}ms`,
+        );
+
+        // console.log(data);
+      }),
+    );
   }
 }
