a882e2ba9b6014492456057e85eecc849b98a736
luisitcho <luishenrisc1@gmail.com>
2025-06-28 04:44:59 -0300

    curso nest - Interceptors | criando um cache simples

---
+++ 1_conceitos_nest/src/common/interceptors/simple-cache.interceptor.ts
@@ -0,0 +1,29 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, of, tap } from 'rxjs';
+
+export class SimpleCacheInterceptor implements NestInterceptor {
+  private readonly cache = new Map();
+
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Promise<Observable<any>> {
+    console.log('SimpleCacheInterceptor: Request started');
+    const request = context.switchToHttp().getRequest();
+    const url = request.url;
+
+    if (this.cache.has(url)) {
+      console.log('SimpleCacheInterceptor: Returning cached response', url);
+      return of(this.cache.get(url));
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulating a delay
+
+    return next.handle().pipe(
+      tap((data) => {
+        this.cache.set(url, data);
+        console.log('SimpleCacheInterceptor: Caching response', url);
+      }),
+    );
+  }
+}
--- 1_conceitos_nest/src/recados/recados.controller.ts
+++ 1_conceitos_nest/src/recados/recados.controller.ts
@@ -21,6 +21,7 @@ import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
 import { ErrorHandlingInterceptor } from 'src/common/interceptors/error-handling.interceptor';
+import { SimpleCacheInterceptor } from 'src/common/interceptors/simple-cache.interceptor';
 
 // CRUD
 // Create -> POST -> Criar um recado
@@ -43,6 +44,7 @@ import { ErrorHandlingInterceptor } from 'src/common/interceptors/error-handling
 //   para: string;
 // }
 
+@UseInterceptors(SimpleCacheInterceptor)
 @Controller('recados')
 @UsePipes(ParseIntIdPipe)
 export class RecadosController {
